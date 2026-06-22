77e621640dbb2ae65be81329d731d3ae2a5c3e06
Madhav Khatod <madhavkhatod8@gmail.com>
2026-06-22 20:37:30 +0530

    Add lockBoard feature to prevent multiple clicks

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 var imgs = [
-'🐶','🐱','🦊','🐰',
-'🦄','🐬','🦋','🐼',
-'🐶','🐱','🦊','🐰',
-'🦄','🐬','🦋','🐼'
+    '🐶','🐱','🦊','🐰',
+    '🦄','🐬','🦋','🐼',
+    '🐶','🐱','🦊','🐰',
+    '🦄','🐬','🦋','🐼'
 ];
 
 var firstCard = "";
 var firstEmoji = "";
 var moves = 0;
 var pairs = 0;
+var lockBoard = false;
 
+shuffleCards();
 createGame();
+
+function shuffleCards(){
+    imgs.sort(() => Math.random() - 0.5);
+}
 
 function createGame(){
 
@@ -19,43 +25,42 @@ function createGame(){
     for(var i=0;i<imgs.length;i++){
 
         document.getElementById("board").innerHTML +=
-        "<div class='card' id='c"+i+"'>?</div>";
-
+        `<div class="card" id="c${i}">?</div>`;
     }
 
     for(var i=0;i<imgs.length;i++){
 
         document.getElementById("c"+i)
         .addEventListener("click", openCard);
-
     }
 }
 
 function openCard(e){
 
-    var id = e.target.id;
+    if(lockBoard) return;
 
+    var id = e.target.id;
     var num = id.replace("c","");
 
-    if(document.getElementById(id).innerHTML != "?"){
+    if(id === firstCard) return;
+
+    if(document.getElementById(id).innerHTML !== "?"){
         return;
     }
 
     document.getElementById(id).innerHTML = imgs[num];
 
-    if(firstCard == ""){
+    if(firstCard === ""){
 
         firstCard = id;
         firstEmoji = imgs[num];
-
     }
     else{
 
         moves++;
-
         document.getElementById("mv").innerHTML = moves;
 
-        if(firstEmoji == imgs[num]){
+        if(firstEmoji === imgs[num]){
 
             pairs++;
 
@@ -64,28 +69,37 @@ function openCard(e){
             document.getElementById("msg").innerHTML =
             "Nice Match!";
 
-            if(pairs == 8){
+            firstCard = "";
+            firstEmoji = "";
+
+            if(pairs === 8){
 
                 document.getElementById("win").style.display =
                 "block";
 
                 document.getElementById("win").innerHTML =
-                "You Won in " + moves + " Moves!";
+                "🎉 You Won in " + moves + " Moves!";
             }
-
         }
         else{
 
             document.getElementById("msg").innerHTML =
             "Not Match!";
 
-            document.getElementById(firstCard).innerHTML = "?";
+            lockBoard = true;
 
-            document.getElementById(id).innerHTML = "?";
+            setTimeout(function(){
+
+                document.getElementById(firstCard).innerHTML = "?";
+                document.getElementById(id).innerHTML = "?";
+
+                firstCard = "";
+                firstEmoji = "";
+
+                lockBoard = false;
+
+            },800);
         }
-
-        firstCard = "";
-        firstEmoji = "";
     }
 }
 
@@ -96,9 +110,9 @@ function restartGame(){
 
     firstCard = "";
     firstEmoji = "";
-
     moves = 0;
     pairs = 0;
+    lockBoard = false;
 
     document.getElementById("mv").innerHTML = "0";
     document.getElementById("pr").innerHTML = "0";
@@ -109,5 +123,6 @@ function restartGame(){
     document.getElementById("win").style.display =
     "none";
 
+    shuffleCards();
     createGame();
 }
